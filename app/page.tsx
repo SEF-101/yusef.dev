d08a0e8052ad896480fa997a3ade7ca23512e1c4
yusef <131935248+SEF-101@users.noreply.github.com>
2025-04-22 12:47:36 -0500

    Added icon cloud

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/hero";
+import { IconCloudDemo } from "@/components/language-cons";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <section className="py-12 bg-[var(--card)] text-center">
         <h2 className="text-4xl font-bold mb-8">Technologies I Work With</h2>
         <div className="flex flex-wrap justify-center gap-8">
-          
+          <IconCloudDemo />
         </div>
       </section>
     </div>
