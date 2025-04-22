65c6514cd502e24893f20d1a661ba0e578e007b2
yusef <131935248+SEF-101@users.noreply.github.com>
2025-04-22 12:55:40 -0500

    fixed projects page up a bit

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -38,9 +38,9 @@ export default function Projects() {
   return (
     <section className="min-h-screen pt-12">
       <div className="container mx-auto">
-        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+        <h1 className="text-5xl mb-8 xl:mb-16 text-center mx-auto">
           My Projects
-        </h2>
+        </h1>
         <Tabs defaultValue={category} className="mb-24 xl:mb-48">
           <TabsList className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none">
             {uniqueCategories.map((cat, index) => (
@@ -48,7 +48,7 @@ export default function Projects() {
                 key={index}
                 value={cat}
                 onClick={() => setCategory(cat)}
-                className="capitalize w-[162px] md:w-auto"
+                className="capitalize w-[162px] md:w-auto data-[state=active]:bg-accent"
               >
                 {cat}
               </TabsTrigger>
