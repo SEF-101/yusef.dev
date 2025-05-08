b7798fa172ad40fe003a3aac87b50e6a47b4d7fc
yusef <131935248+SEF-101@users.noreply.github.com>
2025-05-08 13:36:10 -0500

    Fixing up project card

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Card, CardHeader } from "./ui/card";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "./ui/card";
 import { Github, Link2Icon } from "lucide-react";
 import { Badge } from "./ui/badge";
 
@@ -16,17 +16,11 @@ interface Project {
 }
 
 const ProjectCard = ({ project }: { project: Project }) => {
-
-  // Create onClick handler for whole card
-  // const handleCardClick = () => {
-  //   router.push(`/projectdetails/${project.id}`); // Use ID for unique routing
-  // };
-
   return (
     <Card className="group overflow-hidden relative">
       <CardHeader className="p-0">
-        {/* image  */}
-        <div className="relative w-full h-[300px] flex  items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat">
+        {/* image */}
+        <div className="relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat">
           <Image
             className="absolute bottom-0 shadow-2xl"
             src={project.image}
@@ -35,32 +29,39 @@ const ProjectCard = ({ project }: { project: Project }) => {
             alt=""
             priority
           />
-          {/* btn links */}
-          <div className="flex gap-x-4">
+        </div>
+        <Badge className="text-sm absolute top-4 left-5 bg-accent text-primary">
+          {project.category}
+        </Badge>
+      </CardHeader>
+      
+      <CardContent className="px-8 py-6">
+        <CardTitle className="mb-1">{project.name}</CardTitle>
+        <CardDescription className="text-lg">{project.description}</CardDescription>
+      </CardContent>
+      
+      <CardFooter className="px-8 flex justify-start">
+        <div className="flex gap-x-4">
+          {project.link && (
             <Link
               target="_blank"
               href={project.link}
-              className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200"
+              className="w-[42px] h-[42px] rounded-full flex justify-center items-center"
             >
-              <Link2Icon className="text-white" />
+              <Link2Icon className="text-white h-5 w-5 hover:text-primary" />
             </Link>
+          )}
+          {project.github && (
             <Link
               target="_blank"
               href={project.github}
-              className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-300"
+              className="w-[42px] h-[42px] rounded-full flex justify-center items-center"
             >
-              <Github className="text-white" />
+              <Github className="text-white h-5 w-5 hover:text-primary" />
             </Link>
-          </div>
+          )}
         </div>
-      </CardHeader>
-      <div className="h-full px-8 py-6 ">
-        <Badge className="text-sm mb-2 absolute top-4 left-5 bg-accent">
-          {project.category}
-        </Badge>
-        <h4 className="h4 mb-1">{project.name}</h4>
-        <p className="text-muted-foreground text-lg">{project.description}</p>
-      </div>
+      </CardFooter>
     </Card>
   );
 };
