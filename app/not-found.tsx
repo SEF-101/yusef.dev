94557ba605321ed8cde0cd7fab31eb99594b392c
yusef <131935248+SEF-101@users.noreply.github.com>
2025-05-30 11:40:35 -0500

    Added 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,11 +4,11 @@ import { Button } from "@/components/ui/button";
 export default function NotFound() {
   return (
     <div className="container mx-auto flex flex-col items-center justify-center min-h-[70vh] px-4 text-center">
-        <h1 className="text-5xl font-bold mb-6 text-[var(--primary)]">404 - Page Not Found</h1>
+        <h1 className="text-5xl  mb-6 text-[var(--primary)]">404 - Page Not Found</h1>
         <p className="text-xl mb-8 text-[var(--primary)]">
-          Sorry, the page you are looking for doesn&apos;t exist or has been moved.
+          Seems you may have taken a wrong turn. No worries, click below to return home.
         </p>
-        <Button asChild size="lg">
+        <Button variant="ghost" size="lg" className="border border-[var(--primary)] hover:bg-[var(--accent)]">
           <Link href="/" className="px-8 py-2">
             Return Home
           </Link>
